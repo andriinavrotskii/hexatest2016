3a1e2d0e2f99a3f937dade85479e67890679f67c
Andrii Navrotskii <andrii.navrotskii@gmail.com>
2016-12-14 13:39:06 +0000

    Add phpDoc

--- src/ImageLoader.php
+++ src/ImageLoader.php
@@ -8,13 +8,33 @@ use Exception;
 */
 class ImageLoader
 {
-
+    /**
+     * Directory where image will be saved
+     *
+     * @var string
+     */
     private $saveToPath;
+
+    /**
+     * Allowed mime file types
+     *
+     * @var array
+     */
     private $allowMimeTypes;
+
+    /**
+     * Rewrite existing file 
+     *
+     * @var bool
+     */
     private $allowRewriteFile;
 
-    
-    function __construct(string $saveToPath = '')
+    /**
+     * Construct method
+     *
+     * @param string $saveToPath (optional)
+     */
+    public function __construct(string $saveToPath = '')
     {
         $this->setSaveToPath($saveToPath);
         $this->setAllowMimeTypes(['image/jpeg', 'image/png', 'image/gif']);
@@ -22,60 +42,13 @@ class ImageLoader
     }
 
     /**
-     * 
-     */
-    public function setSaveToPath(string $path = '')
-    {
-        if (empty($path)) {
-            $path = sys_get_temp_dir();
-        }
-
-        if (substr($path, -1) != DIRECTORY_SEPARATOR) {
-            $path .= DIRECTORY_SEPARATOR;
-        }
-
-        if (is_dir($path)) {
-            $this->saveToPath = $path;
-        } else {
-            throw new Exception("Path not exist - " . $path);
-        }
-    }
-
-
-    public function getSaveToPath()
-    {
-        return $this->saveToPath;
-    }
-
-
-    public function setAllowMimeTypes(array $allowMimeTypes = []) {
-        if (empty($allowMimeTypes)) {
-            throw new Exception("Expect at least one mime type");
-            return;
-        }
-
-        $this->allowMimeTypes = $allowMimeTypes;
-    }
-
-    public function getAllowMimeTypes()
-    {
-        return $this->allowMimeTypes;
-    }
-
-
-    public function setAllowRewriteFile(bool $allowRewriteFile)
-    {
-        $this->allowRewriteFile = $allowRewriteFile;
-    }
-
-    public function getAllowRewriteFile()
-    {
-        return $this->allowRewriteFile;
-    }
-
-
-    /**
+     * Main method - loading image
      *
+     * @param string $url
+     * @param string $fileName (optional)
+     *
+     * @return array Return array if success
+     * @return bool Return false if fail
      */
     public function load($url, $fileName = '')
     {
@@ -107,8 +80,97 @@ class ImageLoader
         }
     }
 
+    /**
+     * Set path for images, if $path is empty - set php temp dir
+     *
+     * @param string $path (optional)
+     * 
+     * @return void
+     */
+    public function setSaveToPath(string $path = '')
+    {
+        if (empty($path)) {
+            $path = sys_get_temp_dir();
+        }
+
+        if (substr($path, -1) != DIRECTORY_SEPARATOR) {
+            $path .= DIRECTORY_SEPARATOR;
+        }
+
+        if (is_dir($path)) {
+            $this->saveToPath = $path;
+        } else {
+            throw new Exception("Path not exist - " . $path);
+        }
+    }
+
+    /**
+     * Get directory for images
+     * 
+     * @return string
+     */
+    public function getSaveToPath()
+    {
+        return $this->saveToPath;
+    }
 
 
+    /**
+     * Set array of allowed mime types
+     *
+     * @param array $allowMimeTypes (optional)
+     * 
+     * @return void
+     */
+    public function setAllowMimeTypes(array $allowMimeTypes = []) {
+        if (empty($allowMimeTypes)) {
+            throw new Exception("Expect at least one mime type");
+            return;
+        }
+
+        $this->allowMimeTypes = $allowMimeTypes;
+    }
+
+    /**
+     * Get allowed mime types
+     * 
+     * @return string
+     */
+    public function getAllowMimeTypes()
+    {
+        return $this->allowMimeTypes;
+    }
+
+    /**
+     * Set enable or disable rewrite file if file with same name is exist
+     *
+     * @param bool $allowRewriteFile (optional)
+     * 
+     * @return void
+     */
+    public function setAllowRewriteFile(bool $allowRewriteFile = true)
+    {
+        $this->allowRewriteFile = $allowRewriteFile;
+    }
+
+    /**
+     * Get allowRewriteFile
+     * 
+     * @return bool
+     */
+    public function getAllowRewriteFile()
+    {
+        return $this->allowRewriteFile;
+    }
+
+
+    /**
+     * Check image before loading. Check headers and image size
+     *
+     * @param string $url
+     *
+     * @return bool 
+     */
     private function checkBeforeLoading(string $url)
     {
         // check headers
@@ -140,12 +202,27 @@ class ImageLoader
     }
 
 
+    /**
+     * Check if mime type in array of allowed mime types
+     * 
+     * @param string $mimeType
+     *
+     * @return bool
+     */
     private function checkAllowMimeTypes(string $mimeType)
     {
         return in_array ($mimeType, $this->allowMimeTypes);
     }
 
 
+    /**
+     * Generate array of path, name, full file path
+     *
+     * @param string $fileName
+     * @param string $mimeType
+     *
+     * @return array
+     */
     private function getSaveTo(string $fileName, string $mimeType)
     {
         $mimeTypeArr = explode('/', $mimeType);
@@ -166,7 +243,13 @@ class ImageLoader
         return $saveTo;
     }
 
-
+    /**
+     * Check file now exist or not and allowed rewrite file or not/
+     *
+     * @param string $file
+     *
+     * @return bool
+     */
     private function checkSaveToFile(string $file)
     {
         if (!file_exists($file)) {
