e3a12707f20104bcb72611df893c90da2d15bef5
Andrii Navrotskii Home <andrii.navrotskii@gmail.com>
2016-12-26 21:10:51 +0000

    rename test to tests

---
+++ tests/Imageloader.test.php
@@ -0,0 +1,37 @@
+<?php
+namespace Andriinavrotskii\Hexatest2016\Test;
+
+use Andriinavrotskii\Hexatest2016\ImageLoader;
+/**
+* Tests for ImageLoader
+*/
+class ImageLoaderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testLoadToPath()
+    {
+
+        $path = __DIR__ . DIRECTORY_SEPARATOR . 'images';
+        $mime = ['image/jpeg', 'image/png', 'image/gif'];
+        $imageLoader = new ImageLoader($path);
+        $imageLoader->setAllowMimeTypes($mime);
+        $imageLoader->setAllowRewriteFile(true);
+
+        $url = 'http://www.w3schools.com/css/img_fjords.jpg';
+        $name = 'my_image';
+        $result = $imageLoader->load($url, $name);
+
+        $expectation = [
+            'path' => __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR,
+            'name' => 'my_image.jpg',
+            'file' => __DIR__ . DIRECTORY_SEPARATOR . 'images'. DIRECTORY_SEPARATOR . 'my_image.png'
+        ];
+        
+        $this->assertEquals($result, $expectation);
+    }
+
+
+
+
+
+}
